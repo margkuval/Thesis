bda96577275ddee266ea5738537e73227a658184
Margarita K <m.kuvaldina@gmail.com>
2018-04-05 15:04:45 +0200

    new tri, focus on mapping

---
+++ python_projects/tri-modif.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""Members characteristics"""
+u = np.array([0, 1, 2])
+v = np.array([3, 4, 5])
+xcoor = np.array([0, 3, 1])
+ycoor = np.array([0, 0, 5])
+
+"""Material characteristics"""
+E = np.array([40000, 40000, 40000])  #modul for each member
+A = np.array([0.01, .02, 0.02])  #area - each member
+

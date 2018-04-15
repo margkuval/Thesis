381a1a7637ed0684a6db1238a87b8664ae265abf
Margarita K <m.kuvaldina@gmail.com>
2018-04-15 17:05:55 +0200

    checked with SW calc

--- python_projects/tri-modif.py
+++ python_projects/tri-modif.py
@@ -19,15 +19,15 @@ yj = ycoord[np.ix_(jEdge)]
 
 numnode = xcoord.shape[0]  #all nodes must be used
 numelem = iEdge.shape[0]  #count # of beginnings
-tdof = 2*numelem  #total degrees of freedom
+tdof = 2*numnode  #total degrees of freedom
 
 "Connectivity MAT computation"
 ij = np.vstack([[2*iEdge, 2*iEdge+1], [2*jEdge, 2*jEdge+1]]).transpose()
 print(ij)
 
-"""Material characteristics E=(MPa), A=(m2)"""
+"""Material characteristics E=(kPa), A=(m2)"""
 E = np.array([40000, 40000, 40000])  #modulus of elasticity for each member
-A = np.array([0.02, .02, 0.02])  #area - each member
+A = np.array([0.0225, .0225, 0.0225])  #area - each member 0.15x0.15
 
 """"Global stiffness MAT"""
 gStif = np.zeros((tdof, tdof))  #empty Global Stiffness MAT
@@ -35,7 +35,7 @@ length = np.sqrt(pow((xj - xi), 2) + pow((yj - yi), 2))
 c = (xj - xi)/length
 s = (yj - yi)/length
 
-for p in range(numelem):
+for p in range(numnode):
     i = iEdge[p]
     j = jEdge[p]
     n = ij[p]
@@ -53,10 +53,10 @@ F = np.zeros((tdof, 1))  #ForcesMAT
 u = np.zeros((tdof, 1))  #deflectionsMAT, 1 = # of columns
 
 "Outside Forces [kN]"
-F[4] = 10
-F[2] = 60
+F[4] = 60
+F[2] = 10
 F_3x2 = F.reshape(3, 2)
-print(F_3x2)
+#print(F_3x2)
 
 "Fixed and active DOFs"
 fixedDof = np.array([0, 1, 3])  #fixed dof
@@ -66,11 +66,6 @@ actDof = np.setdiff1d(np.arange(tdof), fixedDof)  #Return sorted,unique values f
 u1 = np.linalg.solve(gStif[np.ix_(actDof, actDof)], F[np.ix_(actDof)])
 u[np.ix_(actDof)] = u1
 print(u)
-
-"""Reactions"""
-#Reac = gStif*u    #NOT FINISHED
-F = gStif*u + F
-
 
 """Inner forces"""
 k = E*A/length
@@ -87,17 +82,17 @@ stress = Flocal[0]/A
 stress_normed = [i/sum(abs(stress)) for i in abs(stress)]
 print(stress)
 
-xinew = xi + uxi[0]  #BUG-there is an [[ in u array, if changing, need clean whole code, now solved by taking "list 1"from the MAT
+xinew = xi + uxi[0]  #BUG-there is an [[ in u array, if changing, need clean whole code, now solved by taking "list 0"from the MAT
 xjnew = xj + uxj[0]
 yinew = yi + uyi[0]
 yjnew = yj + uyj[0]
 
-"""Plot original structure"""
+"""Plot structure"""
 
 """plt.plot(xi, yi)###withoutFORfun
 plt.plot(xj, yj)"""
 
-for r in range(numelem):
+for r in range(numnode):
     x = (xi[r], xj[r])
     y = (yi[r], yj[r])
     line = plt.plot(x,y)
@@ -110,7 +105,7 @@ for r in range(numelem):
     xnew = (xinew[r], xjnew[r])
     ynew = (yinew[r], yjnew[r])
     linenew = plt.plot(xnew, ynew)
-    plt.setp(linenew, label='strain' if stress[r] > 0 else 'stress', ls='-', c='crimson' if stress[r] > 0 else 'c', lw=1+6*stress_normed[r])
+    plt.setp(linenew, label='strain' if stress[r] > 0 else 'stress', ls='-', c='c' if stress[r] > 0 else 'crimson', lw=1+20*stress_normed[r])
 
 plt.axis('equal')
 plt.xlabel('meters')

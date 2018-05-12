472b5169ead14013aec2393a9a17bf3a9f4cc129
Margarita K <m.kuvaldina@gmail.com>
2018-05-12 14:18:49 +0200

    will try to implement on a larger structure, so it is more clear where mistake lies

---
+++ python_projects/truss_1x3_brain.py
@@ -0,0 +1,14 @@
+import truss_1x3_GA as GA
+
+task = GA.GA(10)  # num of different results
+task.initial()
+for i in range(10):  # num of cycles
+    task.calc()
+    task.fitness()
+    if i % 5 == 0:
+        task.plot()
+    task.crossover()
+    task.mutate(mutation_type="x")
+    task.mutate(mutation_type="a")
+    task.mutate(mutation_type="y")
+    task.mutate_worst()

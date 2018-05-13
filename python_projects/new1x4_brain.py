e491fa2d13c200fdebcec7cff830ce252eead210
Margarita K <m.kuvaldina@gmail.com>
2018-05-13 11:56:26 +0200

    mutation change of how often it runs it

--- python_projects/new1x4_brain.py
+++ python_projects/new1x4_brain.py
@@ -2,13 +2,15 @@ import new1x4_GA as GA
 
 task = GA.GA(100)  # num of different results
 task.initial()
-for i in range(200):  # num of cycles
+for i in range(201):  # num of cycles
     task.calc()
     task.fitness()
     if i % 100 == 0:
         task.plot()
     task.crossover()
-    task.mutate(mutation_type="x")
-    task.mutate(mutation_type="a")
-    task.mutate(mutation_type="y")
-    task.mutate_worst()
+    if i % 30 == 0:
+        task.mutate(mutation_type="x")
+        task.mutate(mutation_type="a")
+    if i % 40 == 0:
+        task.mutate(mutation_type="y")
+        task.mutate_worst()
